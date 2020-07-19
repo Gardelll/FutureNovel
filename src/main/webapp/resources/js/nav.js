9b4d369a64cd5b00e28918dba9ecd7f340bd8c55
十 <337828925@qq.com>
2020-07-19 16:47:51 +0800

    注册

--- src/main/webapp/resources/js/nav.js
+++ src/main/webapp/resources/js/nav.js
@@ -1,16 +1,4 @@
 window.onload = function() {
-    //				var direction = document.getElementById("direction");
-    //				var first = document.getElementById("first");
-    //				var timer =null;
-    //				clearInterval(timer);
-    //				    timer = setTimeout(function() {
-    //				        first.onmouseover = function(){
-    //							direction.style.transform = "rotateX(180deg)"
-    //						};
-    //						first.onmouseout = function(){
-    //							direction.style.transform = "rotateX(0deg)"
-    //						};
-    //				    },1000)
 
     function displaywindowsSize() {
         var w = document.documentElement.clientWidth;
@@ -25,31 +13,13 @@ window.onload = function() {
     }
     window.addEventListener("resize", displaywindowsSize);
     displaywindowsSize();
-};window.onload = function() {
-    //				var direction = document.getElementById("direction");
-    //				var first = document.getElementById("first");
-    //				var timer =null;
-    //				clearInterval(timer);
-    //				    timer = setTimeout(function() {
-    //				        first.onmouseover = function(){
-    //							direction.style.transform = "rotateX(180deg)"
-    //						};
-    //						first.onmouseout = function(){
-    //							direction.style.transform = "rotateX(0deg)"
-    //						};
-    //				    },1000)
 
-    function displaywindowsSize() {
-        var w = document.documentElement.clientWidth;
-        var h = document.documentElement.clientHeight;
-        var topBoxCenter = document.getElementById("top-box-center");
-        if (w < 1200) {
-            topBoxCenter.style.width = 990 + "px";
-        }
-        if (w > 1200) {
-            topBoxCenter.style.width = 1200 + "px";
-        }
-    }
-    window.addEventListener("resize", displaywindowsSize);
-    displaywindowsSize();
+
+    // var first = document.getElementById("first");
+    // var classify_list = document.getElementById("classify-list");
+
+    // first.onclick = function() {
+    //     classify_list.style.display = classify_list.style.display == "block" ? "none" : "block";
+    // }
+
 };
--- src/main/webapp/resources/js/reg_click_tip.js
+++ src/main/webapp/resources/js/reg_click_tip.js
@@ -3,7 +3,9 @@ window.onclick = function click_tip() {
     var txtphonepwd = document.getElementById("txtphonepwd");
     var password_tip = document.getElementsByClassName("password-tip")[0];
 
+
     txtphonepwd.onclick = function() {
         password_tip.style.display = "block";
     }
+
 }

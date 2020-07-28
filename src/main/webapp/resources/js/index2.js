d9fa844865139c8d7406734c8eea5b6528578b83
xiaowang <1577142422@qq.com>
2020-07-29 02:44:43 +0800

    在改亿点点小问题

--- src/main/webapp/resources/js/index2.js
+++ src/main/webapp/resources/js/index2.js
@@ -32,23 +32,27 @@ function popup_user(id){
                             <span class="">
                                 <input type="text" class="" id="popup_user" name="user" placeholder="输入用户名"></span></div>
                                 <div class="item">
-                                    <label class="">邮箱</label>
+                                    <label class="">账号状态</label>
                                     <span class="">
-                                        <input type="text" class="" id="popup_user" name="user" placeholder="输入邮箱"></span></div>
+                                    <select name="status" class="item" id="popup_status" οnchange="checkinfo_location();" >
+                                    <option value="FINE" >正常</option>
+                                    <option value="UNVERIFIED" >未验证</option>
+                                    <option value="BANED" >封禁</option>
+                                    </select>
                                         <div class="item">
                                             <label class="">手机号</label>
-                                            <span class=""><input type="text" class="" id="popup_user" name="user" placeholder="输入用户名"></span></div>
+                                            <span class=""><input type="text" class="" id="popup_phone" name="user" placeholder="输入用户名"></span></div>
                                             <div class="item">
                                                 <label class="">头像</label>
                                                 <span class="">
-                                                    <input type="text" class="" id="popup_user" name="user" placeholder="请上传图片">
+                                                    <input type="text" class="" id="popup_head" name="user" placeholder="请上传图片">
                                                     </span>
                                                     <span class="">
-                                                        <button te>上传图片</button>
+                                                        <button>上传图片</button>
                                                         </span>
                                                 </div></form></div>
                                                 <span style="bottom: 10px; right: 15px; position: absolute;">
-                                                    <button type="button" class="btn" id="submit-user" style="margin-right: 10px;" onclick="paging_mod(this,\''+id+'\');$(\'.popup_mod\').remove()">提交</button>
+                                                    <button type="button" class="btn" id="submit-user" style="margin-right: 10px;" onclick="user_mod(${id});$(\'.popup_mod\').remove()">提交</button>
                                                     <button type="button" class="btn" onClick="$(\'.popup_mod\').remove();">取消</button></span></div>`)
 }
 //批量添加用户弹窗
@@ -83,29 +87,55 @@ function popup_user_add(){
                                                         <button type="button" class="btn" id="submit-user" style="margin-right: 10px;" onclick="user_add();$(\'.popup_mod\').remove()">提交</button>
                                                         <button type="button" class="btn" onClick="$(\'.popup_mod\').remove();">取消</button></span></div>`)
 }
+
 //批量添加用户ajax
 function user_add(){
-    let userName=$('#popup_user').val(),
+    let Arr=[],
+        userName=$('#popup_user').val(),
         userEmail=$('popup_email').val(),
         password=$('popup_pas').val(),
         phone=$('popup_phone').val();
+        let data1={"userName": userName,"password": password,"email": userEmail,"phone": phone
+        };
+        Arr.push(data1);
         $.ajax({
             url:/*[[${#request.getContextPath()} + '/api/admin/account/add']]*/'http://localhost:8080/future-novel/api/admin/account/add',
             type: 'post',
             datatype:'json',
-            data: JSON.stringify({
-                userName: userName,
-                password: password,
-                email: userEmail,
-                phone: phone
-            }),
+            data: JSON.stringify(Arr),
             contentType: 'application/json; charset=utf-8',
             success: function(data){
                 console.log('添加成功');
             },
             error: function(jqXHR){
                 console.log('添加失败');
-                console.log(jqXHR.responseText.failedt)
+                console.log(jqXHR.responseJSON.errorMessage);
+            }
+        })
+}
+//修改用户接口函数
+function user_mod(id){
+    let userName=$('#popup_user').val(),
+        userStatus=$('#popup_status').find('option:selected').val(),
+        head=$('popup_head').val(),
+        phone=$('popup_phone').val();
+        $.ajax({
+            url:/*[[${#request.getContextPath()} + '/api/account/edit']]*/'http://localhost:8080/future-novel/api/account/edit',
+            type: 'GET',
+            
+            data: {
+                uuid: id,
+                userName: userName,
+                status: userStatus,
+                phone: Number(phone)
+            },
+            success: function(data){
+                popup_over('icon-happy-l','#1afa29','添加成功');
+            },
+            error: function(jqXHR){
+                let data = JSON.parse(jqXHR.responseText);
+                popup_over('icon-sad','#d81e06',data.errorMessage);
+                console.log(data.errorMessage);
             }
         })
 }

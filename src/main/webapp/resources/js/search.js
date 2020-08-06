20f624f38e7a00821b090158764b73b0751acb42
litong <1473179073@qq.com>
2020-08-06 18:57:44 +0800

    QAQ

--- src/main/webapp/resources/js/search.js
+++ src/main/webapp/resources/js/search.js
@@ -1,40 +1,4 @@
 $(function() {
-    function GetTags() {
-        $.ajax({
-            url: "http://localhost:8080/future-novel/api/novel/tags/all",
-            type: "get",
-            data: "",
-            success: function(data) {
-                console.log(data);
-                for (var i = 0; i < data.length; i++) {
-                    $("#data").append("<option value='" + "" + data[i] + "" + "'>")
-                }
-            },
-            error: function(data) {
-                console.log(data.errorMessage)
-            }
-        });
-    }
-    GetTags();
-
-    function GetSeries() {
-        $.ajax({
-            url: "http://localhost:8080/future-novel/api/novel/series/all",
-            type: "get",
-            data: "",
-            success: function(data) {
-                console.log(data);
-                for (var i = 0; i < data.length; i++) {
-                    $("#data").append("<option value='" + "" + data[i] + "" + "'>")
-                }
-            },
-            error: function(data) {
-                console.log(data.errorMessage)
-            }
-        });
-    }
-    GetSeries();
-
     function displaywindowsSize() {
         var w = document.documentElement.clientWidth;
         var h = document.documentElement.clientHeight;
@@ -51,10 +15,6 @@ $(function() {
             $("#main-content-wrap").css("width", 1200 + "px");
         }
     }
-
-    $("#fixed-input").keydown(function() {
-
-    })
     window.addEventListener("resize", displaywindowsSize);
     displaywindowsSize();
 

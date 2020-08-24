d8f36a1a57da5d05ffabdedaece90a35236ff740
litong <1473179073@qq.com>
2020-08-24 23:17:30 +0800

    改了一些bug

--- src/main/resources/static/resources/js/writer.js
+++ src/main/resources/static/resources/js/writer.js
@@ -3,42 +3,6 @@ window.onload = function () {
     $("#modify-novel").click(function () {
         $(".modify-Novel").css("top",0);
     })
-    var tags = [];
-    var actor=["haha","heihei"];
-    $("#book-tags").keydown(function (event) {
-        if(event.keyCode==32){
-            if($("#book-tags").val().length>=4){
-                alert("标签不得超过三个字")
-                $("#book-tags").val("");
-            }else{
-                var str= $("#book-tags").val();
-                var test= " <div class=\"tags-s\">\n" +
-                    "<a href=\"###\" class=\"tags-text\">"+str+"</a>\n" +
-                    "<i class=\"fa fa-close close2\" ></i>\n" +
-                    "</div>";
-                $(".tags").append(test);
-                $("#book-tags").val("");
-            }
-        }
-    });
-    $(".tags").on('click','.close2',function(){
-        $(this).parent().remove();
-    });
-    function GetTags(){
-        $.ajax({
-            url:"http://localhost:8080/future-novel/api/novel/tags/all",
-            type:"get",
-            data:"",
-            success:function (data) {
-                for (var i=0;i<data.length;i++){
-                    $("#tag").append(" <option value='" + "" + data[i] + "" + "'>")
-                }
-            },error:function (data) {
-                console.log(data)
-            }
-        });
-    }
-    GetTags();
     $("#file").on("change",function () {
         var formData = new FormData ;
         formData.append("file",$("#file")[0].files[0]);
